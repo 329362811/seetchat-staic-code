abb25277b3c7615d913be2f98f96874f78b3e8b1
longjianwen0826 <1073512789@qq.com>
2020-07-20 19:38:59 +0800

    个人中心更新

--- src/pages/index5/index.vue
+++ src/pages/index5/index.vue
@@ -1,16 +1,53 @@
 <template>
 
-    <div class="main">
-
-        <van-nav-bar
-                :title='title'
-                fixed
-        />
-
-        <div class="content">
-                <img :src="img1" class="img" @click="gomyActivities">
-                <img :src="img2" class="img" @click="gomyperformance">
-				<img :src="img3" class="img" @click="goResearch">
+    <div class="container">
+        <div class="woDe">
+            我的个人中心
+        </div>
+        <div class="ziLiaoKa">
+            <van-row gutter="24">
+                <van-col span="5">
+                    <van-image round class="touXiang" src="https://img.yzcdn.cn/vant/cat.jpeg" />
+                </van-col>
+                <van-col span="16">
+                    <txt class="niCheng">零度</txt>
+                    <txt class="idHao">ID号：12151463674</txt>
+                </van-col>
+                <van-col span="3">
+                    <van-icon class="jianTou" name="arrow" />
+                </van-col>
+            </van-row>
+        </div>
+        <div class="caiFuKa">
+            <van-row class="caiFuColumnOne" gutter="24">
+                <van-col span="24">
+                    <txt class="oneLeft">我的明细</txt>
+                    <a href="#" class="oneRight" style="float:right">明细</a>
+                </van-col>
+                <van-col span="20">
+                    <txt class="oneLeftTwo">24小时后可提现</txt>
+                </van-col>
+            </van-row>
+            <van-row class="caiFuColumnTwo" gutter="24">
+                <txt class="twoLeft">6100</txt>
+                <txt class="twoRight" style="float:right">
+                    <van-button plain round type="primary" class="twoRightBut">提现</van-button>
+                </txt>
+            </van-row>
+        </div>
+        <div class="gongNengKa">
+            <a href="#"> 
+                <van-row class="gongNengLan" gutter="24">
+                    <div class="gongNengLanLeft"><van-icon name="wap-nav" size="28" color="#1DE7A7" /></div>
+                    <div class="gongNengLanRight">我的需求</div>
+                </van-row>
+            </a> 
+            <a href="#"  @click="keFuBut"> 
+                <van-row class="gongNengLan" gutter="24">
+                    <div class="gongNengLanLeft"><van-icon name="wap-nav" size="28" color="#1DE7A7" /></div>
+                    <div class="gongNengLanRight">客服咨询 09:00-22:00</div>
+                </van-row>
+            </a> 
         </div>
 
 
@@ -28,18 +65,13 @@
     import TabBar from '@/components/tabBar';
     import { Dialog} from 'vant';
     import Vue from 'vue';
-    Vue.use(Dialog);
     export default {
 
         //基础数据存放处
         data (){
             return {
-                title : '我的',
-                img1: require('@/assets/images/other/4c429533dfa2d23c0a40430e92c8601.jpg'),
-                img2: require('@/assets/images/other/wodeyeji.jpg'),
-				img3: require('@/assets/images/other/8021574236234_.pic_hd.jpg'),
-                //  screenWidth: document.body.clientWidth, // 屏幕尺寸
-                //  screenHeight: document.body.clientHeight, // 屏幕尺寸
+               
+               
             }
         },
 
@@ -50,61 +82,28 @@
 
         //网页加载完成
         mounted () {
-            const that = this
-            window.onresize = () => {
-                return (() => {
-                    window.screenWidth = document.body.clientWidth;
-                    window.screenHeight = document.body.clientHeight;
-                    that.screenWidth = window.screenWidth;
-                    that.screenHeight = window.screenHeight;
-
-                })()
-            }
+            
         },
 
         //声明方法
         methods : {
-
-            gomyActivities : function(){
-                Dialog.confirm({
-                    message: "请选择身份",
-                    //closeOnClickOverlay: true,
-                    confirmButtonText: "普惠营销管理岗",
-                    cancelButtonText: "营销主管岗" ,
-                    cancelButtonColor:"#1989fa",
-                }).then(()=> {
-                    sessionStorage.setItem("offlineHuoDong_active","主管岗_已发布") //确认新建
-                    sessionStorage.setItem("offlineHuoDongActive","已发布") //去到待审核-
-                    sessionStorage.setItem("intelligenceEnablement","management")
-                    sessionStorage.setItem("saveCg","false") //默认保存草稿为false
-                    this.$router.push('/offlineHuoDong');
-                }).catch(() => {
-                    sessionStorage.setItem("offlineHuoDong_active","主管岗_已发布") //确认新建
-                    sessionStorage.setItem("offlineHuoDongActive","已发布") //去到待审核-
-                    sessionStorage.setItem("intelligenceEnablement","supervisor")
-                    sessionStorage.setItem("saveCg","false") //默认保存草稿为false
-                    this.$router.push('/offlineHuoDong');
-                })
-                // Dialog.confirm({
-                //     message: "请选择身份",
-                //     confirmButtonText: "管理岗", //改变确认按钮上显示的文字
-                //     cancelButtonText: "经办岗" ,//改变取消按钮上显示的文字
-                //     cancelButtonColor:"#1989fa",
-                // }).then(()=> {
-                //     sessionStorage.setItem("identity","management")
-                //     this.$router.push('/myActivities');
-                // }).catch(() => {
-                //     sessionStorage.setItem("identity","handle")
-                //     this.$router.push('/myActivities');
-                // })
-            },
-            gomyperformance : function(){
-                this.$router.push('./myperformance');
-            },
-			goResearch : function(){
-			    this.$router.push('./research');
-			},
-
+            //客服咨询是否拨打电话
+            keFuBut: function(){
+                Dialog.confirm(
+                    {
+                        title: '确定拨打电话',
+                        message: '400-8161-666',
+                        confirmButtonText: '拨打',
+                        cancelButtonText: "取消",
+                        confirmButtonColor: "#1DE7A7",
+                    }).then(() => {
+                        //alert("确定")
+                    }).catch(() => {
+                        //alert("取消")
+                    }
+                );
+            }
+           
 
         },
 
@@ -120,19 +119,79 @@
     }
 </script>
 
-<style lang="scss" scoped>
-    .img{
-        border-radius: 20px;
-        width: 95%;
-        padding:8px 12px 0 12px;
-    };
-    .main{
-        background-color: rgb(238,238,238);
-        margin: 0;
-        padding: 0;
-    };
-    .content{
-        height:600px;
-        background-color: rgb(238,238,238);
-    }
+<style lang="stylus" scoped>
+    .container
+        padding:0px 15px;
+        .woDe
+            height:60px
+            line-height :80px;
+            font-weight :bold
+            font-size:23px
+        .ziLiaoKa
+            height:100px
+            .touXiang  
+                float:left
+                width:70px
+                height:70px
+                border-radius:50vw
+                margin-top:15px;
+            .niCheng
+                float:left
+                font-size:14px
+                font-weight:bold
+                width:200px;
+                margin-top:28px
+                margin-bottom:7px;
+            .idHao
+                float:left
+                font-size:12px
+            .jianTou
+                margin-top:10vw
+                float right
+        .caiFuKa
+            height:150px
+            padding:15px 15px 0 20px
+            border-radius:15px
+            box-shadow:0px 0px 3px 3px rgba(0,0,0,0.2)
+            // background: url(https://img.yzcdn.cn/vant/cat.jpeg) repeat-x center
+            .caiFuColumnOne
+                line-height:24px
+                .oneLeft
+                    float:left
+                    font-size:16px
+                .oneRight
+                    float:right
+                    font-size:14px
+                .oneLeftTwo
+                    float:left
+                    font-size:14px
+                    margin-left:0px
+            .caiFuColumnTwo
+                margin-top:10px
+                line-height:90px
+                .twoLeft
+                    float:left
+                    font-size:50px
+                .twoRight
+                    float:right
+                    .twoRightBut
+                        margin-top:33px
+                        height:36px
+                        font-size:16px
+        .gongNengKa
+            height:200px
+            margin-top:25px
+            .gongNengLan
+                padding-bottom:10px
+                margin-top:15px
+                .gongNengLanLeft
+                    float:left
+                    width:30px
+                    height:26px
+                    border:1px dotted
+                .gongNengLanRight
+                    float:left
+                    padding-top:3px
+                    color:#000000
+                    margin-left:20px
 </style>
